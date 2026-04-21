acf663b889a8d7bf00bd3838d5912eee30a1ee91
Skywing608 <VenableN0879@my.sduhsd.net>
2026-04-21 19:41:57 +0000

    Added a past recommendation table

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ let output = document.getElementById("output");
 let btnOutput = document.getElementById("btnOutput");
 let btnReset = document.getElementById("btnReset");
 let recommendation = "";
+let pastRec1 = document.getElementById("pastRec1");
+let pastRec2 = document.getElementById("pastRec2");
+let pastRec3 = document.getElementById("pastRec3");
+let pastRec4 = document.getElementById("pastRec4");
+let pastRec5 = document.getElementById("pastRec5");
+
+let pastRecommendations = [];
 
 let clothing = [
   {name: "A heavy jacket", warmth: "heavy"},
@@ -54,6 +61,14 @@ function reset() {
     }
     
     output.textContent = "";
+    
+    pastRecommendations = [];
+    pastRec1.textContent = "_";
+    pastRec2.textContent = "_";
+    pastRec3.textContent = "_";
+    pastRec4.textContent = "_";
+    pastRec5.textContent = "_";
+
 }
 
 btnOutput.addEventListener("click", function() {
@@ -71,9 +86,21 @@ btnOutput.addEventListener("click", function() {
     }
     else{
         output.textContent = "Hi " + userName.value + "! We recommend you wear: " + recommendation;
+        
+        pastRecommendations.push({temp: temperature, rec: recommendation});
+        if (pastRecommendations.length > 3) {
+            pastRecommendations.shift();
+        }
+        pastRec1.textContent = pastRecommendations[0] ? `Temp: ${pastRecommendations[0].temp}°F - ${pastRecommendations[0].rec}` : "_";
+        pastRec2.textContent = pastRecommendations[1] ? `Temp: ${pastRecommendations[1].temp}°F - ${pastRecommendations[1].rec}` : "_";
+        pastRec3.textContent = pastRecommendations[2] ? `Temp: ${pastRecommendations[2].temp}°F - ${pastRecommendations[2].rec}` : "_";
+        pastRec4.textContent = pastRecommendations[3] || "_";
+        pastRec5.textContent = pastRecommendations[4] || "_";
     }
 });
 
 btnReset.addEventListener("click", function() {
     reset();
 });
+
+
